593c3da1108093b6dad59d96fef8a8ce04ca9b5b
congtrung6391 <congtrung6391@gmail.com>
2024-03-19 23:49:27 +0700

    update theme neovim

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -2,7 +2,7 @@ return {
   -- Color theme
   {
     "folke/tokyonight.nvim",
-    lazy = false,
+    lazy = true,
     opts = { style = "moon" },
     enabled = false,
   },
@@ -13,20 +13,20 @@ return {
       vim.g.everforest_better_performance = 1
       vim.cmd.colorscheme [[everforest]]
     end,
-    enabled = false,
+    enabled = true,
   },
   {
     'sainnhe/sonokai',
-    lazy = false,
+    lazy = true,
     config = function()
       vim.g.sonokai_style = "atlantis"
       vim.cmd.colorscheme [[sonokai]]
     end,
-    enabled = true,
+    enabled = false,
   },
   {
     'shaunsingh/nord.nvim',
-    lazy = false,
+    lazy = true,
     config = function()
       vim.g.nord_contrast = true
       vim.g.nord_borders = false
